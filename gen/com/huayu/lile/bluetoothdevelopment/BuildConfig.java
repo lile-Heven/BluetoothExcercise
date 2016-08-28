66c96cb7d2e3ef5ec9f0e33427600c774be83fd5
huayu.lile <315059124@qq.com>
2016-08-28 22:25:32 +0800

    This is BluetoothDevelopment.

---
+++ gen/com/huayu/lile/bluetoothdevelopment/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.huayu.lile.bluetoothdevelopment;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
